cc46f21e59d2907fad7d1f4de0e69b390abdbe0d
emilio ovalles-misterman <eomisterman@gmail.com>
2022-12-15 17:48:59 -0500

    spotify scopes update

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,8 +1,18 @@
 const redirectURI = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://playmakerspotify.com/';
 const clientID = '5084645426d2429a8ef352a99ba328b3';
+const scopes = [
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'streaming',
+    'user-modify-playback-state',
+    'user-read-playback-state',
+    'user-library-read',
+    'user-read-private',
+    'user-read-email'
+]
 
 const Spotify = {
-    accessUrl: `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`,
+    accessUrl: `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=${scopes.join(' ')}&redirect_uri=${redirectURI}`,
 
     search(term) {
         const accessToken = window.localStorage.getItem('token');
@@ -22,7 +32,8 @@ const Spotify = {
                 name: track.name,
                 artist: track.artists[0].name,
                 album: track.album.name,
-                uri: track.uri
+                uri: track.uri,
+                preview: track.preview_url
             }));
         });
     },
