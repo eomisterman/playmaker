0ec68e0d5b66ca610c5bd729cc8b6e64d2059d55
emilio ovalles-misterman <eomisterman@gmail.com>
2023-01-19 16:06:28 -0500

    Display top songs within song browser.

--- src/Components/SongBrowser/SongBrowser.js
+++ src/Components/SongBrowser/SongBrowser.js
@@ -1,15 +1,43 @@
 import './SongBrowser.css';
-import React from "react";
+import React, { useState } from "react";
+import Tracklist from '../Tracklist/Tracklist';
+import Spotify from '../../util/Spotify';
 
 const SongBrowser = (props) => {
+    const [view, setView] = useState("");
+
+    const [songs, setSongs] = useState([]);    
+    const showSongs = () => {
+        setView("songs");
+        Spotify.top("tracks").then(result => {
+            setSongs(result);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    const changeView = (event) => {
+        setView(event.target.value);
+    }
+
+    const [ artists, setArtists] = useState([]);
+    const showArtists = () => {
+        setView("artists");
+        
+    }
+
+
     return (
-        <ul className='SongBrowser'>
-            <li>Your Playlists</li>
-            <li>Top Songs</li>
-            <li>Top Artists</li>
-            <li>Genre</li>
-            <li>Search</li>
-        </ul>
+        <div>
+            <ul className='SongBrowser'>
+                <li onClick={changeView} value="playlists">Your Playlists</li>
+                <li onClick={showSongs} value="songs">Top Songs</li>
+                <li onClick={changeView} value="artists">Top Artists</li>
+                <li onClick={changeView} value="genres">Genres</li>
+                <li onClick={changeView} value="search">Search</li>
+            </ul>
+            {view === "songs" && <Tracklist tracks={songs} onAdd={props.onAdd} isRemoval={false}/>}
+        </div>
     );
 }
 
