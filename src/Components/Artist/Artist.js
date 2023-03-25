fe77a7ea313defd71f9f1923925b2f91338a5ae7
emilio ovalles-misterman <eomisterman@gmail.com>
2023-03-25 19:35:42 -0400

    fix warning messages related to keys

--- src/Components/Artist/Artist.js
+++ src/Components/Artist/Artist.js
@@ -3,13 +3,19 @@ import React from "react";
 
 const Artist = (props) => {
     return (
+
+
         <div className='Artist' id={props.id} key={props.id}>
             <img src={props.images.at(-2).url} alt={props.name} />
             <div id="artist-details">
                 <h3>{props.index+1}. {props.name}</h3>
-                {props.genres.map(genre => <p>{genre}</p>)}
+                {props.genres.map((genre, index) => (
+                    <p key={index}>{genre}</p>
+                ))}
             </div>
         </div>
+
+
     );
 }
 
